f6beaf89bc943b9dca4d6691b7d680a66a1e92b3
Gaspare Mascolino <gaspare.mascolino@mail.com>
2024-07-25 15:21:50 +0200

    android

--- lib/components/contacts/contact_buttons.dart
+++ lib/components/contacts/contact_buttons.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class ContactButtons extends StatelessWidget {
-
   const ContactButtons({
     super.key,
   });
 
   static const buttons = {
-    'Phone': 'assets/images/contacts/phone.svg',
-    'WhatsApp': 'assets/images/contacts/whatsapp.svg',
-    'Instagram': 'assets/images/contacts/instagram.svg',
+    'Phone': {
+      'icon': 'assets/images/contacts/phone.svg',
+      'onTap': 'mailto:gasparemascolino@icloud.com',
+    },
+    'WhatsApp': {'icon': 'assets/images/contacts/whatsapp.svg'},
+    'Instagram': {'icon': 'assets/images/contacts/instagram.svg'},
   };
 
+  void _handleTap(String label) async {
+    final button = buttons[label];
+    if (button != null && button.containsKey('onTap')) {
+      final url = button['onTap']!;
+      launchUrlString(url);
+
+    } else {
+      print('Tapped on $label');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,20 +36,21 @@ class ContactButtons extends StatelessWidget {
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: buttons.entries.map((entry) {
-            return Column(
-              children: [
-                SvgPicture.asset(
-                  entry.value,
-                  width: 60,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
-                  child: Text(entry.key, style: Theme
-                      .of(context)
-                      .textTheme
-                      .bodySmall),
-                ),
-              ],
+            return GestureDetector(
+              onTap: () => _handleTap(entry.key),
+              child: Column(
+                children: [
+                  SvgPicture.asset(
+                    entry.value['icon']!,
+                    width: 60,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10.0),
+                    child: Text(entry.key,
+                        style: Theme.of(context).textTheme.bodySmall),
+                  ),
+                ],
+              ),
             );
           }).toList(),
         )
--- lib/components/contacts/location.dart
+++ lib/components/contacts/location.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:sisdandify/data/contacts_data.dart' as data;
 
 class Location extends StatelessWidget {
+  static const String sisMap = 'assets/images/contacts/sis-map.jpg';
   const Location({
     super.key,
   });
+
+  void _launchURL() async {
+    final Uri url = Uri.parse('https://maps.app.goo.gl/4EUfPh6bYkpQ8sCB9');
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +24,11 @@ class Location extends StatelessWidget {
       Text(data.locationLabel, style: Theme.of(context).textTheme.titleMedium),
       const SizedBox(height: 20),
       Text(data.addressLabel, style: Theme.of(context).textTheme.bodySmall),
+      const SizedBox(height: 20),
+      GestureDetector(
+        onTap: _launchURL,
+        child: Image.asset(sisMap, width: 600),
+      ),
     ]);
   }
 }
